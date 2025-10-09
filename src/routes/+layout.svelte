2a26c82d351fd8ae2823457b524c5ef148ca7552
vlOd <66838724+vlOd2@users.noreply.github.com>
2025-10-10 02:18:49 +0300

    Fix bg animation + add cursor glow

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,30 @@
     import Navbar from "$lib/components/Navbar.svelte";
     import { page } from "$app/state";
     import { blur } from "svelte/transition";
+
     let { children } = $props();
+    let cursorGlow: HTMLDivElement;
+
+    function onmousemove(e: MouseEvent) {
+        cursorGlow.style.setProperty("--mouse-y", e.clientY.toFixed(2));
+        cursorGlow.style.setProperty("--mouse-x", e.clientX.toFixed(2));
+    }
+
+    function onmousedown(e: MouseEvent) {
+        cursorGlow.classList.add("active");
+    }
+
+    function onmouseup(e: MouseEvent) {
+        cursorGlow.classList.remove("active");
+    }
 </script>
 
 <svelte:head>
     <title>vlOd's website</title>
 </svelte:head>
+
+<svelte:document {onmousemove} {onmousedown} {onmouseup} />
+<div class="cursor-glow" bind:this={cursorGlow}></div>
 
 <div class="body-flex">
     <Navbar />
