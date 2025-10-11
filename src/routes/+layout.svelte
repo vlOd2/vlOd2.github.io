ea24a68c8963a22c8d8373a527dd4b342d025f14
vlOd <66838724+vlOd2@users.noreply.github.com>
2025-10-11 22:09:13 +0300

    Rework cursor glow to work when scrolling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,6 +10,14 @@
 
     let { children } = $props();
     let content = $state<HTMLDivElement>();
+    let glow = $state<HTMLDivElement>();
+
+    function updateGlow() {
+        if (!content || !glow) {
+            return;
+        }
+        glow.style.top = `${content.scrollTop}px`;
+    }
 
     function onmousemove(e: MouseEvent) {
         if (!content) {
@@ -18,8 +26,14 @@
         const rect = content.getBoundingClientRect();
         const x = e.clientX - rect.left;
         const y = e.clientY - rect.top;
-        content.style.setProperty("--mouse-x", `${x}px`);
-        content.style.setProperty("--mouse-y", `${y}px`);
+        content.style.setProperty("--glow-x", `${x}px`);
+        content.style.setProperty("--glow-y", `${y}px`);
+        updateGlow();
+        if (x < 0 || y < 0 || x > rect.width || y > rect.height) {
+            content.style.setProperty("--glow-opacity", "0");
+        } else {
+            content.style.setProperty("--glow-opacity", "1");
+        }
     }
 </script>
 
@@ -32,7 +46,8 @@
 
     <div class="content-container" {onmousemove}>
         {#key page.route.id}
-            <div class="content" bind:this={content} in:blur>
+            <div class="content" bind:this={content} onscroll={() => updateGlow()} in:blur>
+                <div class="cursor-glow" bind:this={glow}></div>
                 {@render children?.()}
             </div>
         {/key}
